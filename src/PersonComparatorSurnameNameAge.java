59300190749b834c103c9682f8c875dfc7c03079
Ildar Gilmutdinov <ir.gilmutdinov@mail.ru>
2022-09-24 23:30:29 +0400

    first commit

--- src/PersonComparatorSurnameNameAge.java
+++ src/PersonComparatorSurnameNameAge.java
@@ -11,12 +11,12 @@ public class PersonComparatorSurnameNameAge implements Comparator<Person> {
 
     @Override
     public int compare(Person p1, Person p2) {
-        int surname1 = p1.getSurname().split(" ").length;
-        int surname2 = p2.getSurname().split(" ").length;
+        int surname1 = p1.getSurname().split(" ").length; // разделяем фамилии на слова
+        int surname2 = p2.getSurname().split(" ").length; // разделяем фамилии на слова
 //        if (Math.min(surname1, maxSurname) != (Math.min(surname2, maxSurname))) {
-        if ((surname1 >= maxSurnameWords) != (surname2 >= maxSurnameWords)) {
+        if ((surname1 >= maxSurnameWords) != (surname2 >= maxSurnameWords)) { //условие 1, сравниваем наших людей
             return Integer.compare(surname1, surname2);
         }
-        return Integer.compare(p1.getAge(), p2.getAge());
+        return Integer.compare(p1.getAge(), p2.getAge()); // сравнение по возрасту
     }
 }
